cc9df21745a3119129268bdc8ecb743083bf7afe
deleterium <rui.santana@protonmail.com>
2026-05-21 16:28:48 -0300

    Refactor: `brs.modals.block` to Typescript. All done.

--- src/js/brs/brs.modals.block.ts
+++ src/js/brs/brs.modals.block.ts
@@ -14,25 +14,26 @@ import {
 } from './brs.util'
 
 import { getTransactionDetails } from './brs.transactions'
+import { GetBlockResponse, Transaction } from '../typings'
 
-export function evBlocksTableClick (event) {
+export function evBlocksTableClick (event: Event) {
     event.preventDefault()
     if (BRS.fetchingModalData) {
         return
     }
     BRS.fetchingModalData = true
-    const blockHeight = $(this).data('block')
+    const blockHeight = $(event.target as HTMLElement).data('block')
     sendRequest('getBlock+', {
         height: blockHeight,
         includeTransactions: 'true'
-    }, function (response) {
+    }, function (response: GetBlockResponse) {
         showBlockModal(response)
     })
 }
 
-export function showBlockModal (block) {
-    $('#block_info_modal_block').html(String(block.block).escapeHTML())
-    const blockDetails = $.extend({}, block)
+export function showBlockModal (block: GetBlockResponse) {
+    $('#block_info_modal_block').text(block.block)
+    const blockDetails = $.extend({}, block) as any
     delete blockDetails.transactions
     delete blockDetails.previousBlockHash
     delete blockDetails.nextBlockHash
@@ -47,24 +48,28 @@ export function showBlockModal (block) {
         return
     }
     $('#block_info_transactions_none').hide()
-    $('#block_info_transactions_table').show()
-    block.transactions.sort(function (a, b) {
+    $('#block_info_transactions_table').show();
+    (block.transactions as Transaction[]).sort(function (a: Transaction, b: Transaction) {
         return a.timestamp - b.timestamp
     })
     let rows = ''
-    for (const transaction of block.transactions) {
+    for (const transaction of block.transactions as Transaction[]) {
         const details = getTransactionDetails(transaction)
-        rows += '<tr>'
-
-        rows += "<td><a href='#' data-transaction='" + String(transaction.transaction).escapeHTML() + "'>" + String(transaction.transaction.slice(0, 7) + '…').escapeHTML() + '</a><br>'
-        rows += details.nameOfTransaction + '</td>'
-        rows += '<td>' + details.senderHTML + '</td>'
-        rows += '<td>' + details.recipientHTML + '</td>'
-        rows += `<td ${details.colorClass}>${details.amountToFromViewerHTML}</td>`
-        rows += '<td>' + formatNQTAsAmount(transaction.feeNQT) + '</td>'
-        rows += '</tr>'
+        const transactionId = String(transaction.transaction).escapeHTML()
+        const shortId = String(transaction.transaction.slice(0, 7) + '…').escapeHTML()
+        rows += `
+            <tr>
+                <td>
+                    <a href='#' data-transaction='${transactionId}'>${shortId}</a><br>
+                    ${details.nameOfTransaction}
+                </td>
+                <td>${details.senderHTML}</td>
+                <td>${details.recipientHTML}</td>
+                <td ${details.colorClass}>${details.amountToFromViewerHTML}</td>
+                <td>${formatNQTAsAmount(transaction.feeNQT)}</td>
+            </tr>`
     }
-    $('#block_info_transactions_table tbody').empty().append(rows)
+    $('#block_info_transactions_table tbody').html(rows)
     $('#block_info_modal').modal('show')
     BRS.fetchingModalData = false
 }
