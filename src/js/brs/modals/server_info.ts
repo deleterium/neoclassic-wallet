f37a67147e4b4fa6051da42b276252784effd0c8
deleterium <rui.santana@protonmail.com>
2026-06-27 21:44:15 -0300

    Refactor: modal `server_info` using `sendRequestA`

--- src/js/brs/modals/server_info.ts
+++ src/js/brs/modals/server_info.ts
@@ -1,34 +1,33 @@
 import { BRS } from '..'
 
-import { sendRequest } from '../core/send_request'
+import { sendRequestA } from '../core/send_request'
 
 import { formatVolume, formatNQTAsAmount, formatTimestampAsDateTime } from '../core/numbers'
 
 import { GetStateResponse } from '../typings'
 
-export function evBrsModalServerInfoOnShowBsModal() {
+export async function evBrsModalServerInfoOnShowBsModal() {
     if (BRS.fetchingModalData) {
         return
     }
 
     BRS.fetchingModalData = true
 
-    sendRequest('getState', {}, function (state: GetStateResponse) {
-        BRS.fetchingModalData = false
+    const state: GetStateResponse = await sendRequestA('getState', {})
+    BRS.fetchingModalData = false
 
-        for (const key in state) {
-            const el = $('#brs_node_state_' + key)
-            if (el.length) {
-                if (key.indexOf('number') !== -1) {
-                    el.html(formatNQTAsAmount(state[key]))
-                } else if (key.indexOf('Memory') !== -1) {
-                    el.html(formatVolume(state[key]))
-                } else if (key === 'time') {
-                    el.html(formatTimestampAsDateTime(state[key]))
-                } else {
-                    el.html(String(state[key]).escapeHTML())
-                }
+    for (const key in state) {
+        const el = $('#brs_node_state_' + key)
+        if (el.length) {
+            if (key.indexOf('number') !== -1) {
+                el.html(formatNQTAsAmount(state[key]))
+            } else if (key.indexOf('Memory') !== -1) {
+                el.html(formatVolume(state[key]))
+            } else if (key === 'time') {
+                el.html(formatTimestampAsDateTime(state[key]))
+            } else {
+                el.html(String(state[key]).escapeHTML())
             }
         }
-    })
+    }
 }
