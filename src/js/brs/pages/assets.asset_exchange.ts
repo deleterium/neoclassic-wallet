faa6b6e17c3b7191244e7cddf69c7ec04838f577
deleterium <rui.santana@protonmail.com>
2026-07-30 18:49:39 -0300

    Fix: add property 'mintable' in asset exchange.

--- src/js/brs/pages/assets.asset_exchange.ts
+++ src/js/brs/pages/assets.asset_exchange.ts
@@ -414,6 +414,7 @@ function loadAsset(asset: DBAsset, refreshHTML: boolean, refreshAsset: boolean) 
         $('#asset_name').html(asset.name)
         $('#asset_description').html(asset.description)
         $('#asset_quantity').html(formatQNTAsQuantity(asset.quantityCirculatingQNT, asset.decimals))
+        $('#asset_mintable').text(asset.mintable ? $.t('yes') : $.t('no'))
 
         $('.asset_name').html(asset.name)
         $('#sell_asset_button').data('asset', assetId)
