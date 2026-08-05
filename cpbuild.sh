86bd9dd7e5ba4b524675d917f69226ca5b0d8d79
deleterium <rui.santana@protonmail.com>
2026-08-04 21:30:05 -0300

    Refactor: using `fontawesome` from npm package.

--- cpbuild.sh
+++ cpbuild.sh
@@ -1,6 +1,8 @@
 rm -Rf dist/
 mkdir -p dist dist/js/3rdparty/ dist/css/
-cp -r src/css src/fonts src/img dist/
+cp -r src/css src/img dist/
 cp node_modules/admin-lte/dist/js/adminlte.* dist/js/3rdparty/
 cp node_modules/admin-lte/plugins/bootstrap/js/bootstrap.bundle.* dist/js/3rdparty/
 cp node_modules/admin-lte/dist/css/adminlte.* dist/css/
+cp node_modules/@fortawesome/fontawesome-free/css/all.min.css dist/css/
+cp -r node_modules/@fortawesome/fontawesome-free/webfonts dist/
--- cpdev.sh
+++ cpdev.sh
@@ -1,3 +1,3 @@
-cp -r src/css src/fonts src/img dist/
+cp -r src/css src/img dist/
 cp dist/index.html dist/index.dev.html
 sed -i 's/index.min.js/index.dev.js/g' dist/index.dev.html
