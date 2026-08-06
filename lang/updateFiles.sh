cf9eb398f4aa6a332749f1887feab5ae84d3e0f6
deleterium <rui.santana@protonmail.com>
2026-08-06 09:06:52 -0300

    Refactor: readarray to loop for more compatability.

--- lang/updateFiles.sh
+++ lang/updateFiles.sh
@@ -3,18 +3,21 @@
 INPUT="translations.csv"
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
 
+# Read the entire file into a variable
+content=$(<"$INPUT")
+
+# Process the content in memory
 IFS=""
 newline=$'\n'
 
 startUp() {
     languagesLength=${#HEADER[@]}
-    for (( j=0; j<${languagesLength}; j++ ));
-    do
+    for (( j=0; j<${languagesLength}; j++ )); do
         # Remove trailing newline
         HEADER[$j]="${HEADER[$j]//$'\n'/}"
         if [ "$j" -ne 0 ]; then
             # Overwrite old file and start json document
-            echo  "{" > ${HEADER[$j]}.json
+            echo  "{" > "${HEADER[$j]}.json"
             FIRSTUSE[$j]=0
         fi
     done
@@ -22,8 +25,7 @@ startUp() {
 }
 
 i=0
-while read LINE
-do
+while read LINE; do
     let i++
     if [ "$i" -eq 1 ]; then
         # Process header. This always adds a newline for all fields
@@ -37,8 +39,7 @@ do
     READYLINE="${PRELINE//,,/,\"\",}"
     # Read fields. This always adds a newline for all fields
     readarray FIELDS < <(echo -n "$READYLINE" | awk -F'","|^"|"$|",|,"|,$' '{for(i=2; i<=NF; i++){printf("%s\n", $i)}}')
-    for (( j=0; j<${languagesLength}; j++ ));
-    do
+    for (( j=0; j<${languagesLength}; j++ )); do
         # Remove trailing newline
         FIELDS[$j]="${FIELDS[$j]//$'\n'/}"
         if [ "$j" -eq 0 ] || [ -z "${FIELDS[$j]}" ]; then
@@ -53,14 +54,13 @@ do
         FIRSTUSE[$j]=1
         output="${output}    \"${FIELDS[0]}\": \"${FIELDS[$j]}\""
         # Append transalation file
-        echo -n -E "${output}" >> ${HEADER[$j]}.json
+        echo -n -E "${output}" >> "${HEADER[$j]}.json"
     done
-done < $INPUT
+done <<< "$content"
 
 # Append the ending } to translation file
-for (( j=1; j<${languagesLength}; j++ ));
-do
-    echo "${newline}}" >> ${HEADER[$j]}.json
+for (( j=1; j<${languagesLength}; j++ )); do
+    echo "${newline}}" >> "${HEADER[$j]}.json"
 done
 
 # Delete this files that was generated in startUp()
